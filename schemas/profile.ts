8790ffcfb0d93ba79bdc8201959dcd06e1061bf1
earthflex <earthflexible@gmail.com>
2023-05-26 15:19:07 +0700

    fix : word

--- schemas/profile.ts
+++ schemas/profile.ts
@@ -156,7 +156,7 @@ export default defineType({
         },
         defineField({
             name: 'pdf',
-            title: 'PDF',
+            title: 'Resume PDF File',
             type: 'file',
             description: 'Upload PDF files.',
             options: {
